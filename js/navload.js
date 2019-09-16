139753261870034a30bb331255436fd82f1fb514
Darshan Krishnaswamy <darsh797@gmail.com>
2019-09-16 13:16:59 -0400

    Restructured directories to separate html, css, and js files

---
+++ js/navload.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('#navdiv').load('navbar.html').fadeOut(1).slideDown(1000);
+    $('.centerinparent').fadeOut(1).slideDown(1000);
+});
--- src/indexjs.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    $('h1').fadeOut(1).slideDown(1200);
-    $('h2').fadeOut(1).slideDown(1200);
-})
