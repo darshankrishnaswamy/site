9ca7314115abcd34f1d8fae1e9c97963223604e3
Darshan Krishnaswamy <darsh797@gmail.com>
2020-01-08 13:57:26 -0500

    Finished Physics

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,4 +1,4 @@
 // Created with help from w3 schools, where I was able to learn how to use Jquery https://www.w3schools.com/jquery/default.asp
 $(document).ready(function(){
-    $('.id1').fadeOut(1).slideToggle(1000);
+    $('.id1').fadeOut(1).slideToggle(1);
 });
--- js/navload.js
+++ js/navload.js
@@ -1,9 +1,9 @@
 // Created with help from w3 schools, where I was able to learn how to use Jquery https://www.w3schools.com/jquery/default.asp
 $(document).ready(function(){
     $('.img').ready(function(){
-        $('#navdiv').load('navbar.html').fadeOut(1).slideDown(1000);
-        $('.centerinparent').fadeOut(1).slideDown(1000);
-        $('.text').fadeOut(1).slideDown(1000);
-        $('.img').fadeOut(1).slideDown(1000);
+        $('#navdiv').load('navbar.html').fadeOut(1).fadeIn(1);
+        $('.centerinparent').fadeOut(1).fadeIn(1);
+        $('.text').fadeOut(1).fadeIn(1);
+        $('.img').fadeOut(1).fadeIn(1);
     });
 });
